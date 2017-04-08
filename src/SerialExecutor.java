39928d690b1b9da33856624a5cb1064e948d17d1
Raketa <detroi.vv@gmail.com>
2017-04-08 21:54:14 +0300

    Map Task Validator

--- src/SerialExecutor.java
+++ src/SerialExecutor.java
@@ -8,29 +8,42 @@ import java.util.*;
 
 public class SerialExecutor<T> implements Executor<T> {
 
-  private List<Task<? extends T>> tasks = new ArrayList<>();
+  /*Валидатор типизирован по Object  иможет быть использован с любыми объектами*/
+  private static final Validator<Object> DEFAULT_VALIDATOR = value -> true;
+
+  private List<TaskAndValidator<T>> tasks = new ArrayList<>();
   private List<T> validResults = new ArrayList<T>();
   private List<T> invalidResults = new ArrayList<T>();
 
-  private Map<Task<? extends T>, Validator<? super T>> map = new HashMap<>();
 
   @Override
   public void addTask(Task<? extends T> task) {
-    tasks.add(task);
+    tasks.add(new TaskAndValidator<T>(task, null));
   }
 
   @Override
   public void addTask(Task<? extends T> task, Validator<? super T> validator) {
     /*Дизайнерская задача, задача ассоциированая с валидатором MAP */
-
+    tasks.add(new TaskAndValidator<T>(task, validator));
+/*Сложность в том что надо синхронизировать две коллекции, они должны иметь синхрорнные состояния
+* И еще task - mutable объект и его кто от может изменить, и поиск по hashcode-у - ничего не принесет*/
   }
 
   /*Давайте имплентировать дальше*/
   @Override
   public void execute() {
-    for (Task<? extends T> task : tasks) {
+    for (TaskAndValidator<T> taskAndValidator : tasks) {
+      Task<? extends T> task = taskAndValidator.task;
       task.execute();
-      validResults.add(task.getResult());
+      if (taskAndValidator.validator != null) {
+        if (taskAndValidator.validator.isValid(task.getResult())) {
+          validResults.add(task.getResult());
+        } else {
+          invalidResults.add(task.getResult());
+        }
+      } else {
+        validResults.add(task.getResult());
+      }
     }
   }
 
@@ -42,5 +55,17 @@ public class SerialExecutor<T> implements Executor<T> {
   @Override
   public List<T> getInvalidResults() {
     return invalidResults;
+  }
+
+  /*Для пары значений ВСЕГДА создавайте класс обертку*/
+  private static class TaskAndValidator<T> {
+    /*можно отойти от бездумного создания get и set*/
+    private Task<? extends T> task;
+    private Validator<? super T> validator;
+
+    public TaskAndValidator(Task<? extends T> task, Validator<? super T> validator) {
+      this.task = task;
+      this.validator = validator;
+    }
   }
 }
